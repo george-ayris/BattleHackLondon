93c0d6e374d959cb2aeb30d5711f864d88ef9f51
George Ayris <george.ayris@huddle.com>
2014-10-12 04:25:50 +0100

    Sorted form being singleton

--- app/viewmodels/newEvent.js
+++ app/viewmodels/newEvent.js
@@ -1,46 +1,47 @@
 define(["durandal/app", "plugins/http", "plugins/router", "knockout"], function (app, http, router, ko) {
-    var vm = {};
-    vm.canActivate = function() {
-        if (app.loggedIn) {
-            return true;
-        }
-        return { redirect:'#/login' };
-    }
-
-    // Want to put a default date in, but bringing in jquery appears to break it
-    //$('inputDate').attr("placeholder",new Date()).format("dd/mm/yy");
-
-    vm.name = ko.observable('');
-    vm.sport = ko.observable('');
-    vm.description = ko.observable('');
-    vm.date = ko.observable('');
-    vm.minPeople = ko.observable('');
-    vm.maxPeople = ko.observable('');
-    vm.cost = ko.observable('');
-
-    vm.createEvent = function() {
-        var createEventUrl = "events"
-        console.log(app.headers)
-        var postResultPromise = http.post(app.rootUrl + createEventUrl,
-            {
-                title: vm.name,
-                activity: vm.sport,
-                description: vm.description,
-                date: vm.date,
-                min: vm.minPeople,
-                max: vm.maxPeople,
-                price: vm.cost
-            },
-            app.headers);
-        postResultPromise.done( function(resp) {
-            console.log("post success", resp);
-            if (resp.status_code === 201) {
-                router.navigate('#/dashboard');
+    var returnVM = function () {
+        var vm = this;
+        vm.canActivate = function() {
+            if (app.loggedIn) {
+                return true;
             }
-        });
-        postResultPromise.fail( function(resp) { console.log("event post failed", resp) } );
-        return postResultPromise;
-    };
+            return { redirect:'#/login' };
+        }
 
-    return vm;
+        // Want to put a default date in, but bringing in jquery appears to break it
+        //$('inputDate').attr("placeholder",new Date()).format("dd/mm/yy");
+
+        vm.name = ko.observable('');
+        vm.sport = ko.observable('');
+        vm.description = ko.observable('');
+        vm.date = ko.observable('');
+        vm.minPeople = ko.observable('');
+        vm.maxPeople = ko.observable('');
+        vm.cost = ko.observable('');
+
+        vm.createEvent = function() {
+            var createEventUrl = "events"
+            console.log(app.headers)
+            var postResultPromise = http.post(app.rootUrl + createEventUrl,
+                {
+                    title: vm.name,
+                    activity: vm.sport,
+                    description: vm.description,
+                    date: vm.date,
+                    min: vm.minPeople,
+                    max: vm.maxPeople,
+                    price: vm.cost
+                },
+                app.headers);
+            postResultPromise.done( function(resp) {
+                console.log("post success", resp);
+                if (resp.status_code === 201) {
+                    router.navigate('#/dashboard');
+                }
+            });
+            postResultPromise.fail( function(resp) { console.log("event post failed", resp) } );
+            return postResultPromise;
+        };
+    };
+    return returnVM;
 });
